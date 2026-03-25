a929841fa47ee8453611ac8b5ee2a27810297e89
Mr. Baguetter <Mr.baguetter1@gmail.com>
2026-03-25 09:38:40 -0500

    - Removed unused references

--- ThaumielMapEditor/API/Helpers/FileManager.cs
+++ ThaumielMapEditor/API/Helpers/FileManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using LabApi.Loader.Features.Paths;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
