a7fb09e89c9b88052cfbf3ad9d2d6414f42ad6b3
Mr. Baguetter <Mr.baguetter1@gmail.com>
2026-03-31 09:04:56 -0500

    - Added schematic LOD blacklist - Added LOD1 zone

--- ThaumielMapEditor/API/Helpers/LODHelper.cs
+++ ThaumielMapEditor/API/Helpers/LODHelper.cs
@@ -8,12 +8,22 @@ namespace ThaumielMapEditor.API.Helpers
     {
         public static void GenerateLODZones(SchematicData schematic)
         {
+            if (Main.Instance.Config.SchematiclodBlacklist.Contains(schematic.FileName))
+                return;
+
             BoxCollider lod0collider = schematic.Primitive.GameObject.AddComponent<BoxCollider>();
             lod0collider.size = schematic.Primitive.Scale*2;
             lod0collider.name = $"{schematic.FileName}-LOD0-Collider";
             lod0collider.isTrigger = true;
             LODZone lod0 = lod0collider.gameObject.AddComponent<LODZone>();
             lod0.Init(schematic, [PrimitiveType.Capsule, PrimitiveType.Sphere]);
+
+            BoxCollider lod1collider = schematic.Primitive.GameObject.AddComponent<BoxCollider>();
+            lod1collider.size = schematic.Primitive.Scale*2.5f;
+            lod1collider.name = $"{schematic.FileName}-LOD1-Collider";
+            lod1collider.isTrigger = true;
+            LODZone lod1 = lod1collider.gameObject.AddComponent<LODZone>();
+            lod1.Init(schematic, [PrimitiveType.Cylinder]);            
         }
     }
 }
