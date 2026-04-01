8d136a04a1591637ff14359a7ef77216169f6692
Mr. Baguetter <Mr.baguetter1@gmail.com>
2026-04-01 10:05:50 -0500

    Removed unused log upload code from LogManager

--- ThaumielMapEditor/Config.cs
+++ ThaumielMapEditor/Config.cs
@@ -8,8 +8,6 @@ namespace ThaumielMapEditor
     public class Config
     {
         public bool Debug { get; set; }
-        public string LogsWebhook { get; set; } = string.Empty;
-        public string APIToken { get; set; } = string.Empty;
         public bool EnableCreditTags { get; set; } = true;
         public List<string> SchematiclodBlacklist { get; set; } = [];
 
