174183220b431c00e1a84ddeb9bc0cd4870ebd12
Mr. Baguetter <Mr.baguetter1@gmail.com>
2026-04-29 09:45:46 -0500

    Forgot to rename ClientSideObjectBase file

--- ThaumielMapEditor/API/Blocks/ClientSide/ClientObject.cs
+++ ThaumielMapEditor/API/Blocks/ClientSide/ClientObject.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="ClientSideObjectBase.cs" company="Thaumiel Team">
+// <copyright file="ClientObject.cs" company="Thaumiel Team">
 // Copyright (c) Thaumiel Team. All rights reserved.
 // Licensed under the GNU General Public License v3.0 (GPL-3.0).
 // </copyright>
